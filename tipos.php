79a89f94d866abe6e48f98f2cf7aefcc1c2095ac
getwes <wesleyfreire1707@gmail.com>
2021-05-14 21:26:01 -0300

    incluindo tipos de dados

---
+++ tipos.php
@@ -0,0 +1,18 @@
+<?php
+
+$idade = 21;
+
+$salario = 1000.30;
+
+$divisao = 10 / 3;
+
+echo gettype ($divisao) ;
+
+echo $divisao ;
+------------
+$verdadeiro = true;
+$falso = false;
+
+echo gettype ($verdadeiro) ;
+
+
