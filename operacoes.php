34ce2c219af46f26b8fa45db4fc4aada4ac141cc
getwes <wesleyfreire1707@gmail.com>
2021-05-14 21:02:15 -0300

    incluindo operacoes

---
+++ operacoes.php
@@ -0,0 +1,28 @@
+<?php 
+
+$idade = 18;
+
+$idadedaqui10anos = $idade + 10;
+
+echo $idadedaqui10anos;
+----------------
+$idade = 18;
+
+$idadeha5anos = $idade - 5;
+
+echo $idadeha5anos;
+
+$soma = 2 + 2;
+$subtracao = 2 - 2;
+$multiplicacao = 2 * 2;
+$divisao = 2 / 2;
+
+$doisaocubo = 2 ** 3;
+
+$restodadivisao = 10 % 3;
+
+echo $restodadivisao;
+
+
+
+
