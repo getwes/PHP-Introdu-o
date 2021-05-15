073e68e1dc89c54696b2b8725aab4a1847a85168
getwes <wesleyfreire1707@gmail.com>
2021-05-14 21:59:47 -0300

    atualização 14/05/21

--- strings.php
+++ strings.php
@@ -4,3 +4,16 @@ $idade = 18;
 echo 'olá mundo minha idade é ' . $idade  . 'anos';
 
 echo "olá mundo minha idade é  $idade  anos";
+
+ como quebrar linhas
+
+$idade = 18;
+
+echo "olá mundo!\n";
+
+// caracter especial precisa estar entre "" caracter para quebrar linhas \n
+echo "\n";
+
+echo "olá mundo!" . PHP_EOL;
+
+echo "eu tenho $idade anos";
