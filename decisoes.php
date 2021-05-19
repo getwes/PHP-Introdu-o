e1ca98ed6afadc7023ed9592188255956be23faf
getwes <wesleyfreire1707@gmail.com>
2021-05-19 20:07:09 -0300

    add decisoes

---
+++ decisoes.php
@@ -0,0 +1,15 @@
+<?php
+
+$idade = 18;
+
+echo "Você só pode entrar se tiver a partir de 18 anos" . PHP_EOL;
+
+if ($idade >= 18) {
+
+echo "Você tem $idade anos." . PHP_EOL;
+
+echo "pode entrar\n";
+echo "\n";
+
+}
+echo  "você só tem $idade anos. você não pode entrar";
