f0b4597112ac62a87f84065c4ce0df0441ce397b
getwes <wesleyfreire1707@gmail.com>
2021-05-24 18:49:50 -0300

    atualização

--- decisoes.php
+++ decisoes.php
@@ -2,14 +2,18 @@
 
 $idade = 18;
 
-echo "Você só pode entrar se tiver a partir de 18 anos" . PHP_EOL;
+echo "Você só pode entrar se tiver a partir de 18 anos ou ";
+echo "apartir de 16 anos acompanhado" . PHP_EOL;
 
 if ($idade >= 18) {
 
 echo "Você tem $idade anos." . PHP_EOL;
-
-echo "pode entrar\n";
+echo 'pode entrar';
 echo "\n";
+} else {
+    echo  "você só tem $idade anos. você não pode entrar";
 
 }
-echo  "você só tem $idade anos. você não pode entrar";
+
+echo PHP_EOL;
+echo "ADEUS!";
